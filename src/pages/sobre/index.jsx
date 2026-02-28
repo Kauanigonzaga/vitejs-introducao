04a8736b7e06e908e22089cff475a98ecbef64c3
Kauani Nunes Costa Gonzaga de Melo <nuneskauani47@gmail.com>
2026-02-27 22:46:10 -0300

    introdução

---
+++ src/pages/sobre/index.jsx
@@ -0,0 +1,9 @@
+export default function Sobre() {
+    return (
+        <>
+
+            <h1>Sobre a aula</h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nisl nunc eget nisl. Nullam auctor, nisl eget ultricies tincidunt, nunc nisl aliquam nisl, eget ultricies nisl nunc eget nisl.</p>
+        </>
+    )
+}
